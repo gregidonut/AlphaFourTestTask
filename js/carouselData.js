e3f0affff945b5762305f5806f4ed93ba4a35894
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-10-19 00:16:12 +0800

    styles: adjust the width of the main element to follow the width in the figma design
    It was uncomfortably wide, but looks better now also we want to be as
    faithful to the design as possible

--- js/carouselData.js
+++ js/carouselData.js
@@ -26,60 +26,6 @@ const CAROUSEL_DATA = [
             "Work with the best.",
         "btnText": "Join Integration Network",
     },
-    {
-        "imagePath": "assets/nothing_held_back.png",
-        "imgAltText": "card01",
-        "header": "NHB Facebook Group",
-        "members": 21_349,
-        "caption": "More than a job board. Hire talent. Built a team. Find a job. Get a gig. \n" +
-            "Work with the best.",
-        "btnText": "Join Facebook Group",
-    },
-    {
-        "imagePath": "assets/job_board.png",
-        "imgAltText": "card01",
-        "header": "NHB Job Board",
-        "members": 21_349,
-        "caption": "More than a job board. Hire talent. Built a team. Find a job. Get a gig. \n" +
-            "Work with the best.",
-        "btnText": "Join Job Board",
-    },
-    {
-        "imagePath": "assets/jv.png",
-        "imgAltText": "card01",
-        "header": "NHB Join Venture Network",
-        "members": 21_349,
-        "caption": "More than a job board. Hire talent. Built a team. Find a job. Get a gig. \n" +
-            "Work with the best.",
-        "btnText": "Join Integration Network",
-    },
-    {
-        "imagePath": "assets/nothing_held_back.png",
-        "imgAltText": "card01",
-        "header": "NHB Facebook Group",
-        "members": 21_349,
-        "caption": "More than a job board. Hire talent. Built a team. Find a job. Get a gig. \n" +
-            "Work with the best.",
-        "btnText": "Join Facebook Group",
-    },
-    {
-        "imagePath": "assets/job_board.png",
-        "imgAltText": "card01",
-        "header": "NHB Job Board",
-        "members": 21_349,
-        "caption": "More than a job board. Hire talent. Built a team. Find a job. Get a gig. \n" +
-            "Work with the best.",
-        "btnText": "Join Job Board",
-    },
-    {
-        "imagePath": "assets/jv.png",
-        "imgAltText": "card01",
-        "header": "NHB Join Venture Network",
-        "members": 21_349,
-        "caption": "More than a job board. Hire talent. Built a team. Find a job. Get a gig. \n" +
-            "Work with the best.",
-        "btnText": "Join Integration Network",
-    },
 ]
 
 export default CAROUSEL_DATA;
